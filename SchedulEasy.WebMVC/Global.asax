37044865db944e4172928505f09e601bbced8c67
Mathew-Jacobs <mathew.jacobs.exe@gmail.com>
2018-08-14 19:58:43 -0700

    Add project files.

---
+++ SchedulEasy.WebMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SchedulEasy.WebMVC.MvcApplication" Language="C#" %>
---
+++ SchedulEasy.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SchedulEasy.WebMVC.Startup))]
+namespace SchedulEasy.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
